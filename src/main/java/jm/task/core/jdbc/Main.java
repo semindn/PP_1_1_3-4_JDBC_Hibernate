3e152506ea6a3971f8812105d8a0a92daab9804f
semindn <semindn@yandex.ru>
2022-10-26 12:00:37 +0300

    Практическая задача 1.1.4 v1.1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,12 +1,9 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
-import org.hibernate.Session;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
